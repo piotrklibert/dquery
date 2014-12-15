ebb5d38a202a9596b242cbfaae605f6f1191486e
Piotr Klibert <piotr.klibert@10clouds.com>
2014-12-16 00:52:35 +0100

    A little cleanup in tests

--- dpath/parser.py
+++ dpath/parser.py
@@ -5,9 +5,13 @@ from dpath import (
     select_all_children,
     select_children,
     select_all_descendants,
-    compose_selectors
+    compose_selectors,
+    select_text
 )
 
+ctx = {
+    "text": select_text
+}
 
 start, stop = pp.StringStart(), pp.StringEnd()
 sep = pp.Literal("/").suppress()
@@ -16,14 +20,20 @@ osep = pp.Optional(sep)
 
 descendants = pp.Literal("**")
 children = pp.Literal("*")
-element = pp.Word(pp.alphanums)
-segment = (descendants | children | element)
+element = pp.Word(pp.alphanums + "-_")
+func = pp.Word(pp.alphas, pp.alphanums + "_") + pp.Literal("(") + pp.Literal(")")
+
+condition = pp.Optional(pp.Literal("[") + pp.Literal("]"))
+
+segment = (descendants | children | func | element) + condition
+
 parser = (start + osep + segment + pp.ZeroOrMore(sep + segment) + osep + stop)
 
 
 descendants.setParseAction(lambda _: [select_all_descendants])
 children.setParseAction(lambda _: [select_all_children])
 element.setParseAction(lambda t: [select_children(lambda x: x.named(t[0]))])
+func.setParseAction(lambda toks: [ ctx[toks[0]] ])
 parser.setParseAction(lambda toks: [compose_selectors(*toks)])
 
 
--- dpath/tests.py
+++ dpath/tests.py
@@ -1,3 +1,5 @@
+import unittest
+
 try:
     from betterprint import pprint
 except ImportError:
@@ -15,52 +17,62 @@ from dpath import (
     select_all_children
 )
 
-test_data = [
-    {"a": [3,4,5, {"z": 10}], "z": "zz"},
-    {"b": "uj", "a": {"z": "az"}, "z": "zz"},
-    ["argh", "asssert", "so", "on"],
-]
-
-test_paths = [
-    "0/a/**",
-    "**/a/*",
-    "**/z",
-    "1",
-    "/1/a/z"
-]
-
-tree = nodify("root", test_data)
 
 from parser import query
 
-for path in test_paths:
-    pprint([path, query(path, tree).as_json()])
 
-res = compose_selectors(
-    select_all_descendants,
-    select_children(lambda x: x.named("a")),
-    select_all_children
-)(tree).as_json()
+class TestSequenceFunctions(unittest.TestCase):
 
-res2 = query("**/a/*", tree).as_json()
-print res == res2
+    def setUp(self):
+        self.test_data = [
+            {"a": [3,4,5, {"z": 10}], "z": "zz"},
+            {"b": "uj", "a": {"z": "az"}, "z": "zz"},
+            ["argh", "asssert", "so", "on"],
+        ]
 
-pprint(
-    compose_selectors(select_all_descendants,
-                      make_mapper(lambda x: x.val if is_leaf(x) else []))
+        self.test_paths = [
+            "0/a/**",
+            "**/a/*",
+            "**/z",
+            "1",
+            "/1/a/z"
+        ]
 
-    (tree))
-
-
-
-
-import json
-b = nodify("root", json.load(open("../sample3.json")))
-bb = compose_selectors(
-    select_all_descendants,
-    make_filter(lambda x: x.named("set-cookie")),
-    select_text
-)(b)
+        self.tree = nodify("root", self.test_data)
 
 
-pprint(bb)
+    def test_simple_paths(self):
+        for path in self.test_paths:
+            pprint([path, query(path, self.tree).as_json()])
+
+    def test_simple_compose(self):
+        res = compose_selectors(
+            select_all_descendants,
+            select_children(lambda x: x.named("a")),
+            select_all_children
+        )(self.tree).as_json()
+
+        res2 = query("**/a/*", self.tree).as_json()
+        self.assertEqual(res, res2)
+
+    def test_simple_mapper(self):
+        pprint(
+            compose_selectors(select_all_descendants,
+                              make_mapper(lambda x: x.val if is_leaf(x) else []))
+
+            (self.tree))
+
+    def test_text_selector(self):
+        import json
+        selector = compose_selectors(
+            select_all_descendants,
+            make_filter(lambda x: x.named("set-cookie")),
+            select_text
+        )
+        b = nodify("root", json.load(open("../sample3.json")))
+        self.assertEqual(selector(b), query("**/set-cookie/text()", b))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
